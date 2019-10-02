e14c16750b6dee2472feb3f0eb786f49cc8f9aa4
Leandre <leandre.blanchard@epitech.eu>
2019-10-02 13:36:02 +0900

    [DEV] sprites now takes an offset to their parent

--- include/Shape.hpp
+++ include/Shape.hpp
@@ -11,15 +11,31 @@ class Rectangle : public IComponent, public sf::RectangleShape
       public:
 	Rectangle(const sf::Vector2f &position,
 		  const sf::Vector2f &size = sf::Vector2f(0, 0),
-		  const sf::Color &color = sf::Color::White) noexcept;
-	void display(sf::RenderWindow &) noexcept;
+		  const sf::Color &color = sf::Color::White) noexcept
+	    : sf::RectangleShape(size)
+	{
+		setPosition(position);
+		setFillColor(color);
+	}
+	void display(sf::RenderWindow &window) noexcept
+	{
+		window.draw(*this);
+	}
 };
 
 class Circle : public IComponent, public sf::CircleShape
 {
       public:
 	Circle(const sf::Vector2f &position, float radius = 0,
-	       const sf::Color &color = sf::Color::White) noexcept;
-	void display(sf::RenderWindow &) noexcept;
+	       const sf::Color &color = sf::Color::White) noexcept
+	    : sf::CircleShape(radius)
+	{
+		setPosition(position);
+		setFillColor(color);
+	}
+	void display(sf::RenderWindow &window) noexcept
+	{
+		window.draw(*this);
+	}
 };
 } // namespace sfs
--- include/Sprite.hpp
+++ include/Sprite.hpp
@@ -8,8 +8,33 @@ namespace sfs
 class Sprite : public IComponent, public sf::Sprite
 {
       public:
-	Sprite(const sf::Texture &texture = sf::Texture()) noexcept;
-	void display(sf::RenderWindow &) noexcept;
-	void update(Scene &, GameObject &) noexcept;
+	Sprite(const sf::Texture &texture = sf::Texture(), const sf::Vector2f &offset = sf::Vector2f(0, 0)) noexcept
+	: sf::Sprite(texture), _offset(offset)
+	{
+
+	}
+
+	void display(sf::RenderWindow &window) noexcept
+	{
+		window.draw(*this);
+	}
+
+	void update(Scene &, GameObject &go) noexcept
+	{
+		setPosition(go.getPosition() + _offset);
+	}
+
+	void setOffset(const sf::Vector2f &offset) noexcept
+	{
+		_offset = offset;
+	}
+
+	sf::Vector2f getOffset() const noexcept
+	{
+		return _offset;
+	}
+
+	protected:
+		sf::Vector2f _offset;
 };
 } // namespace sfs
