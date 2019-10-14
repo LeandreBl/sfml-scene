23d17a239e6befab202d03b82d6eaaaa1344b520
jb <jean-baptiste.melet@epitech.eu>
2019-10-14 15:11:31 +0200

    [add] Offset component

---
+++ include/Offset.hpp
@@ -0,0 +1,22 @@
+#pragma once
+
+#include <IComponent.hpp>
+
+namespace sfs
+{
+    class Offset : public sfs::IComponent
+    {
+        public:
+            Offset(const sf::Vector2f &Parentpos, sf::Vector2f offset) noexcept : _parentPos(Parentpos), _offset(offset) {};
+            void update(sfs::Scene &, GameObject &go) noexcept
+            {
+                sf::Vector2f newPos;
+                newPos.x = _parentPos.x + _offset.x;
+                newPos.y = _parentPos.y + _offset.y;
+                go.setPosition(newPos);
+            }
+        protected:
+            const sf::Vector2f &_parentPos;
+            sf::Vector2f _offset;
+    };
+}
