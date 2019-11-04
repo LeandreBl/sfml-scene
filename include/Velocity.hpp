d2ee292afe82dec25cb404018e95e869b93d1983
Leandre <leandre.blanchard@epitech.eu>
2019-11-04 17:33:29 +0900

    [dev] modify velocity way of work

--- include/Velocity.hpp
+++ include/Velocity.hpp
@@ -1,7 +1,8 @@
 #pragma once
 
 #include <SFML/System.hpp>
-#include <IComponent.hpp>
+
+#include "IComponent.hpp"
 
 namespace sfs
 {
@@ -11,25 +12,34 @@ class Velocity : public sfs::IComponent
 	Velocity(const sf::Vector2f &speed = sf::Vector2f(0, 0),
 		 const sf::Vector2f &accel = sf::Vector2f(0, 0)) noexcept
 	    : _speed(speed), _accel(accel){};
-	void setAcceleration(sf::Vector2f &accel) noexcept
+	void addAcceleration(const sf::Vector2f &add) noexcept
+	{
+		_accel += add;
+	}
+	void setAcceleration(const sf::Vector2f &accel) noexcept
 	{
 		_accel = accel;
-	};
+	}
 	sf::Vector2f getAcceleration() const noexcept
 	{
 		return _accel;
-	};
-	void setSpeed(sf::Vector2f &speed) noexcept
+	}
+	void addSpeed(const sf::Vector2f &add) noexcept
+	{
+		_speed += add;
+	}
+	void setSpeed(const sf::Vector2f &speed) noexcept
 	{
 		_speed = speed;
-	};
+	}
 	sf::Vector2f getSpeed() const noexcept
 	{
 		return _speed;
-	};
+	}
 	void update(sfs::Scene &scene, GameObject &go) noexcept
 	{
-		_speed += _accel;
+		_speed.x *= _accel.x;
+		_speed.y *= _accel.y;
 		go.move(_speed * scene.deltaTime());
 	}
 
