baff44e94792afa7f027a27c88bf889e64f922e3
jb <jean-baptiste.melet@epitech.eu>
2019-11-07 14:45:37 +0100

    [add] buttons strings methods

--- include/Button.hpp
+++ include/Button.hpp
@@ -30,6 +30,8 @@ class Button : public UI
 	void setTextColor(const sf::Color &color) noexcept;
 	void setScale(const sf::Vector2f &scale) noexcept;
 	void setRotate(const float &angle) noexcept;
+	void setString(const std::string &str) noexcept {_text.setString(str);};
+	std::string getString() noexcept {return _text.getString();};
 	void start(Scene &scene) noexcept;
 	void update(Scene &scene) noexcept;
 	void onEvent(Scene &scene, const sf::Event &event) noexcept;
