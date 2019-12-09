30b916b56afa33229cb40cc55c40b7153f1de4d2
Leandre <leandre.blanchard@epitech.eu>
2019-12-10 00:27:14 +0900

    [fix] indentation

--- include/Sound.hpp
+++ include/Sound.hpp
@@ -3,18 +3,17 @@
 #include <SFML/Audio.hpp>
 #include <IComponent.hpp>
 
-namespace sfs
-{
-  class Sound : public sfs::IComponent, public sf::Sound
-  {
-  public:
-    Sound(const sf::SoundBuffer &soundBuf, bool loop = false, bool autoPlay = false)  noexcept
-      : sf::Sound(soundBuf)
-    {
-      setLoop(loop);
-      if (autoPlay == true)
-	play();
-    }
-  protected:
-  };
-}
+namespace sfs {
+class Sound : public sfs::IComponent, public sf::Sound {
+      public:
+	Sound(const sf::SoundBuffer &soundBuf, bool loop = false, bool autoPlay = false) noexcept
+		: sf::Sound(soundBuf)
+	{
+		setLoop(loop);
+		if (autoPlay == true)
+			play();
+	}
+
+      protected:
+};
+} // namespace sfs
