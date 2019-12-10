9014eab4bd1ac1f0a990008933157370c16655d0
Leandre <leandre.blanchard@epitech.eu>
2019-12-10 22:41:26 +0900

    [dev] vector functions

---
+++ include/Vector.hpp
@@ -0,0 +1,21 @@
+#pragma once
+
+#include <math.h>
+
+#include <SFML/System.hpp>
+
+namespace sfs {
+template <typename T, typename T2>
+double distance(const sf::Vector2<T> &a, const sf::Vector2<T2> &b) noexcept
+{
+	return sqrt(pow(b.x - a.x, 2), pow(b.y - a.y, 2));
+}
+
+template <typename T, typename T2>
+double angle(const sf::Vector2<T> &a, const sf::Vector2<T2> &b) noexcept
+{
+	return atan2(b.y - a.y, b.x - a.x);
+}
+
+
+} // namespace sfs
